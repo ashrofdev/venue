bb3c7f0ab7465949aa95161e06fe54c63bb94413
First Last <salmanashraf853@gmail.com>
2019-11-25 12:43:18 +0100

    Updates

---
+++ precache-manifest.0edc543e73cc91e391a6427edce41f97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271f0d5588df4ca1d1454ae309ce1b0c",
+    "url": "/venue/index.html"
+  },
+  {
+    "revision": "1c551d3cdd812b594fc4",
+    "url": "/venue/static/css/main.07aa23b0.chunk.css"
+  },
+  {
+    "revision": "0069147a65b81b663b37",
+    "url": "/venue/static/js/2.40dee7d9.chunk.js"
+  },
+  {
+    "revision": "1c551d3cdd812b594fc4",
+    "url": "/venue/static/js/main.68a4039b.chunk.js"
+  },
+  {
+    "revision": "d9ad7ab66678ca3f4401",
+    "url": "/venue/static/js/runtime~main.52039573.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/venue/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
